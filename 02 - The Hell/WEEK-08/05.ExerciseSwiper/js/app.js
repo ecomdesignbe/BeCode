848970748ce73792de02643d4bcf1aae404ed970
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-06-06 16:04:18 +0200

    update

--- 02 - The Hell/WEEK-08/05.ExerciseSwiper/js/app.js
+++ 02 - The Hell/WEEK-08/05.ExerciseSwiper/js/app.js
@@ -1,11 +1,18 @@
-var swiper = new Swiper(".mywiper", {
-  pagination: {
-    el: "swiper-pagination",
-    clickable: true,
-    
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
-
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  grid: {
+    rows: 3,
+  },
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
   },
 });
