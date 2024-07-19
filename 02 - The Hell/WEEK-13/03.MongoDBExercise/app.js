3a3a6a316212cebc8eab883585ad97f6527e87c1
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-07-19 15:04:17 +0200

    testing deploy on vercel

--- 02 - The Hell/WEEK-13/03.MongoDBExercise/app.js
+++ 02 - The Hell/WEEK-13/03.MongoDBExercise/app.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyparser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/list')
+mongoose.connect('mongodb+srv://steve:XQqSDbyK4wtR6T5u@cluster0.lb4kyf3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 app.use(express.json())
 app.set('view engine', ejs)
