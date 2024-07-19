c3bdfdf6023f284a5af3d74bc1f9a8c2df8d1a50
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-07-19 15:47:25 +0200

    Delete vercel-api-todolist directory

--- vercel-api-todolist/model/todo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-const todoSchema = new Schema({
-    todos : {type : String }
-})
-
-mongoose.model('todo',todoSchema)
